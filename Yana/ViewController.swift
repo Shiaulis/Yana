fd7b322136b92cdc15e8e5c388785f27036c6460
Andrius Shiaulis <shiaulis@gmail.com>
2024-05-12 19:41:28 +0300

    Simple setup

--- Yana/ViewController.swift
+++ Yana/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .orange
     }
-
 
 }
 
